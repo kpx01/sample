c9da8018c677f3c907ba12612f45342534c60c81
kpx01 <masa10448@gmail.com>
2015-08-11 11:46:42 +0900

    commit test

---
+++ seisyun/Assets/seisyun/Scripts/FloorControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorControl : MonoBehaviour {
+    public static float WIDTH = 10.0f;
+    public static int BACK_NUM = 5;
+    public GameObject Object;
+
+    private Object ballObject;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float total_width = FloorControl.WIDTH * FloorControl.BACK_NUM;
+
+        Vector3 floor_position = this.transform.position;
+
+        Vector3 camera_position = Camera.main.transform.position;
+
+        if (floor_position.x + total_width / 2.0f < camera_position.x)
+        {
+            floor_position.x += total_width;
+            this.transform.position = floor_position;
+
+            Destroy(ballObject);
+
+            if (Random.Range(0f, 2.0f) >= 0.5f)
+            {
+                ballObject = Instantiate(Object, this.transform.position + new Vector3(Random.Range(-2.0f, 2.0f), Random.Range(0.5f, 4.0f)), Quaternion.identity);
+                ballObject.name = "ballobject";
+            }
+        }
+	}
+}
---
+++ seisyun/Assets/seisyun/Scripts/LightControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightControl : MonoBehaviour {
+    public float time = 0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if(time >= 1f){
+            Color color = new Color();
+            if (GetComponent<Light>().color.g >= 0f)
+            {
+                color.r = GetComponent<Light>().color.r;
+                color.g = GetComponent<Light>().color.g - 0.01f;
+                color.b = GetComponent<Light>().color.b - 0.01f;
+            }
+            else
+            {
+                color.r = GetComponent<Light>().color.r - 0.01f;
+                color.g = GetComponent<Light>().color.g;
+                color.b = GetComponent<Light>().color.b;
+            }
+            GetComponent<Light>().color = color;
+            time = 0f;
+        }
+	}
+}
